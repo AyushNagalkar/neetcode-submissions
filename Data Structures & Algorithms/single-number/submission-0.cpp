b744d2dff9bc6d17353d74db456d758ec674262a
Ayush Nagalkar <161446894+AyushNagalkar@users.noreply.github.com>
2026-06-26 02:53:13 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int temp = 0 ;
+
+        for(auto i : nums){
+            temp = temp^i ;
+        }
+
+        return temp ;
+        
+    }
+};
